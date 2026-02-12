a41397c838e3519234d0164cf1318f67b933443a
muzakkir2045 <muzakkir2045@gmail.com>
2026-02-12 15:46:09 +0530

    Add requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 
-from flask import Flask, render_template, redirect, request, session, abort, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, abort
 from flask_login import LoginManager, login_user, logout_user, login_required,current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime, timedelta, date, time
+from datetime import datetime, timedelta
 from metrics import insights_analyzer
 from models import db, Users, Projects, WorkSession
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 INSTANCE_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "instance"))
 os.makedirs(INSTANCE_PATH, exist_ok=True)
@@ -19,9 +21,13 @@ app = Flask(
     instance_path=INSTANCE_PATH,
     instance_relative_config=True
 )
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(INSTANCE_PATH, "database.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL',
+    'sqlite:///' + os.path.join(BASE_DIR, 'instance/database.db')
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'my_secret_key'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
 
 db.init_app(app)
 
@@ -29,6 +35,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+with app.app_context():
+    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -51,10 +59,10 @@ def dashboard():
 def new_project():
     if request.method == "POST":
         project = Projects(
-            title=request.form.get('title'),
-            description=request.form.get('description', 'No Description'),
-            status=request.form.get('status', 'active'),
-            user_id=current_user.id
+            title = request.form.get('title'),
+            description = request.form.get('description', 'No Description'),
+            status = 'Active',
+            user_id = current_user.id
         )
         db.session.add(project)
         db.session.commit()
@@ -164,12 +172,15 @@ def edit_project(project_id):
             id=project_id,
             user_id=current_user.id
         ).first_or_404()
-
-
     if request.method == "POST":
         project.title = request.form.get('title')
-        project.description = request.form.get('description','No Description')
-        project.status = request.form.get('status','active')
+        project.description = request.form.get('description')
+
+        allowed_statuses = ["Active", "Completed", "Stalled"]
+        selected_status = request.form.get('status')
+
+        if selected_status in allowed_statuses:
+            project.status = selected_status
 
         db.session.commit()
         return redirect('/dashboard')
@@ -213,7 +224,10 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
